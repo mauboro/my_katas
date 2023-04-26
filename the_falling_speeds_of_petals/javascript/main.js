232dc0405ca122ec0f1e67cc74ebd53a86a426b7
mauboro <trying.again999@outlook.com>
2023-04-26 18:48:51 -0300

    adding katas made today

---
+++ the_falling_speeds_of_petals/javascript/main.js
@@ -0,0 +1,6 @@
+function sakuraFall(v) {
+  if (!v || v < 0){
+    return 0;
+  }
+  return 400 / v
+}
---
+++ the_falling_speeds_of_petals/python/main.py
@@ -0,0 +1,3 @@
+def sakura_fall(v):
+    if not v: return 0
+    return 400 / v if (400 / v) > 0 else 0
